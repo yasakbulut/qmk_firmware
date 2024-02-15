029a71e874a0b6239a18d7890457a8107f97488c
Yasa Akbulut <yasakbulut@gmail.com>
2024-02-16 00:36:54 +0100

    [yasa] copy over oled code from crkbd default

--- users/manna-harbour_miryoku/custom_rules.mk
+++ users/manna-harbour_miryoku/custom_rules.mk
@@ -2,6 +2,9 @@
 # https://github.com/manna-harbour/miryoku
 
 RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+
 OLED_ENABLE     = yes
 OLED_DRIVER     = SSD1306
+SRC += oled.c
+
 LTO_ENABLE      = yes
