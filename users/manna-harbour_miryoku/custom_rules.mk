6acb263c850b408b8f7c2f6369a82584c2f3249f
Yasa Akbulut <yasakbulut@gmail.com>
2024-02-16 00:35:36 +0100

    [yasa] copy over some crkbd defaults

--- users/manna-harbour_miryoku/custom_rules.mk
+++ users/manna-harbour_miryoku/custom_rules.mk
@@ -1,3 +1,7 @@
 # Copyright 2019 Manna Harbour
 # https://github.com/manna-harbour/miryoku
 
+RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
+LTO_ENABLE      = yes
