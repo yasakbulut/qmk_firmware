32f228fc9d3ae69c8831f66d58eef6f1cdc01cfd
Yasa Akbulut <yasakbulut@gmail.com>
2024-04-27 16:11:57 +0200

    Decrease tapping term

--- keyboards/crkbd/keymaps/yasakbulut/config.h
+++ keyboards/crkbd/keymaps/yasakbulut/config.h
@@ -2,7 +2,7 @@
 
 // default but used in macros
 #undef TAPPING_TERM
-#define TAPPING_TERM 200
+#define TAPPING_TERM 150
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define QUICK_TAP_TERM 0
