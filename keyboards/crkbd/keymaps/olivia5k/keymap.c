f5197f20172333b1b8cd75f33449b268800166ce
Olivia Thiderman <olivia@thiderman.org>
2024-06-13 09:24:39 +0200

    Add modifier keys to gaming layout, switch F11 and F7

--- keyboards/crkbd/keymaps/olivia5k/keymap.c
+++ keyboards/crkbd/keymaps/olivia5k/keymap.c
@@ -74,16 +74,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                  KC_LPRN,       KC_RPRN,       KC_UNDS,       XXXXX,         XXXXX,         XXXXX
    ),
     [LAYER_FUN] = LAYOUT(
-    XXXXX,         KC_F12,        KC_F7,         KC_F8,         KC_F9,         KC_PSCR,       XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX,        \
-    XXXXX,         KC_F11,        KC_F4,         KC_F5,         KC_F6,         KC_SCRL,       XXXXX,         KC_LSFT,       KC_LCTL,       KC_LALT,       KC_LGUI,       XXXXX,        \
+    XXXXX,         KC_F12,        KC_F11,        KC_F8,         KC_F9,         KC_PSCR,       XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX,        \
+    XXXXX,         KC_F7,         KC_F4,         KC_F5,         KC_F6,         KC_SCRL,       XXXXX,         KC_LSFT,       KC_LCTL,       KC_LALT,       KC_LGUI,       XXXXX,        \
     XXXXX,         KC_F10,        KC_F1,         KC_F2,         KC_F3,         KC_PAUS,       XXXXX,         XXXXX,         XXXXX,         KC_ALGR,       XXXXX,         XXXXX,        \
                                                  KC_APP,        KC_SPC,        KC_TAB,        XXXXX,         XXXXX,         XXXXX
    ),
 
     [LAYER_GAME] = LAYOUT(
-    XXXXX,         KC_Q,          KC_W,          KC_E,          KC_R,          KC_T,          KC_Y,          KC_U,          KC_I,          KC_O,          KC_P,          KC_EQL,       \
-    XXXXX,         KC_A,          KC_S,          KC_D,          KC_F,          KC_G,          KC_H,          KC_J,          KC_K,          KC_L,          KC_SCLN,       KC_QUOT,      \
-    XXXXX,         KC_Z,          KC_X,          KC_C,          KC_V,          KC_B,          KC_N,          KC_M,          KC_COMM,       KC_DOT,        KC_SLSH,       KC_MINS,      \
+    KC_LEFT_CTRL,  KC_Q,          KC_W,          KC_E,          KC_R,          KC_T,          KC_Y,          KC_U,          KC_I,          KC_O,          KC_P,          KC_EQL,       \
+    KC_LEFT_SHIFT, KC_A,          KC_S,          KC_D,          KC_F,          KC_G,          KC_H,          KC_J,          KC_K,          KC_L,          KC_SCLN,       KC_QUOT,      \
+    KC_LEFT_ALT,   KC_Z,          KC_X,          KC_C,          KC_V,          KC_B,          KC_N,          KC_M,          KC_COMM,       KC_DOT,        KC_SLSH,       KC_MINS,      \
                                                  KC_ESC,        KC_SPC,        KC_TAB,        KC_ENT,        KC_BSPC,       DF(LAYER_BASE)
     ),
 
