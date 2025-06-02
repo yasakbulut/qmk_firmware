8a9f96a772e53aabc70317654995be404c5304a3
Yasa Akbulut <yasakbulut@gmail.com>
2025-06-02 15:50:14 +0200

    Move numbers to the right hand, rearrange layers

--- keyboards/crkbd/keymaps/yasakbulut/keymap.c
+++ keyboards/crkbd/keymaps/yasakbulut/keymap.c
@@ -22,7 +22,7 @@ enum {
 #define MOUSE(KEY) LT(LAYER_MOUSE, KEY)
 #define MEDIA(KEY) LT(LAYER_MEDIA, KEY)
 #define NUM(KEY) LT(LAYER_NUM, KEY)
-#define SYM(KEY) LT(LAYER_SYM, KEY)
+#define GAME(KEY) LT(LAYER_GAME, KEY)
 #define FUN(KEY) LT(LAYER_FUN, KEY)
 
 #define GUI(KEY) LGUI_T(KEY)
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,        KC_Q,          KC_W,          KC_E,          KC_R,          KC_T,          KC_Y,          KC_U,          KC_I,          KC_O,          KC_P,          KC_EQL,       \
     XXXXX,         CTL(KC_A),     ALT(KC_S),     GUI(KC_D),     SFT(KC_F),     KC_G,          KC_H,          SFT(KC_J),     GUI(KC_K),     ALT(KC_L),     CTL(KC_SCLN),  KC_QUOT,      \
     XXXXX,         KC_Z,          KC_X,          KC_C,          KC_V,          KC_B,          KC_N,          KC_M,          KC_COMM,       KC_DOT,        KC_SLSH,       KC_MINS,      \
-                                                 MEDIA(KC_ESC), NAV(KC_BSPC),  MOUSE(KC_TAB), SYM(KC_ENT),   NUM(KC_SPC),   FUN(KC_DEL)
+                                                 FUN(KC_ESC),   NAV(KC_BSPC),  NUM(KC_TAB),   MOUSE(KC_ENT), GAME(KC_SPC),  MEDIA(KC_DEL)
     ),
 
     [LAYER_NAV] = LAYOUT(
@@ -49,36 +49,39 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [LAYER_MOUSE] = LAYOUT(
     XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX,        \
-    XXXXX,         KC_LCTL,       KC_LALT,       KC_LGUI,       KC_LSFT,       XXXXX,         KC_MS_L,       KC_MS_D,       KC_MS_U,       KC_MS_R,       XXXXX,         XXXXX,        \
-    XXXXX,         XXXXX,         KC_ALGR,       XXXXX,         XXXXX,         XXXXX,         KC_WH_R,       KC_WH_U,       KC_WH_D,       KC_WH_L,       XXXXX,         XXXXX,        \
-                                                 XXXXX,         XXXXX,         XXXXX,         KC_BTN2,       KC_BTN1,       KC_BTN3
+    XXXXX,         XXXXX,         KC_MS_L,       KC_MS_U,       KC_MS_D,       KC_MS_R,       XXXXX,         KC_LSFT,       KC_LGUI,       KC_LALT,       KC_LCTL,       XXXXX,        \
+    XXXXX,         XXXXX,         KC_WH_R,       KC_WH_D,       KC_WH_U,       KC_WH_L,       XXXXX,         XXXXX,         XXXXX,         KC_ALGR,       XXXXX,         XXXXX,        \
+                                                 KC_BTN3,       KC_BTN1,       KC_BTN2,       XXXXX,         XXXXX,         XXXXX
     ),
 
     [LAYER_MEDIA] = LAYOUT(
-    XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX,         RGB_MOD,       RGB_HUI,       RGB_SAI,       RGB_VAI,       RGB_TOG,       XXXXX,        \
-    XXXXX,         KC_LCTL,       KC_LALT,       KC_LGUI,       KC_LSFT,       XXXXX,         KC_MPRV,       KC_VOLD,       KC_VOLU,       KC_MNXT,       XXXXX,         XXXXX,        \
-    XXXXX,         XXXXX,         KC_ALGR,       XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX,        \
-                                                 XXXXX,         XXXXX,         XXXXX,         KC_MSTP,       KC_MPLY,       KC_MUTE
+    XXXXX,         RGB_MOD,       RGB_HUI,       RGB_SAI,       RGB_VAI,       RGB_TOG,       XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX,        \
+    XXXXX,         XXXXX,         KC_MPRV,       KC_VOLU,       KC_VOLD,       KC_MNXT,       XXXXX,         KC_LSFT,       KC_LGUI,       KC_LALT,       KC_LCTL,       XXXXX,        \
+    XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX,         KC_ALGR,       XXXXX,         XXXXX,         XXXXX,        \
+                                                 KC_MSTP,       KC_MPLY,       KC_MUTE,       XXXXX,         XXXXX,         XXXXX         
     ),
 
     [LAYER_NUM] = LAYOUT(
-    XXXXX,         KC_LBRC,       KC_7,          KC_8,          KC_9,          KC_RBRC,       XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX,        \
-    XXXXX,         KC_SCLN,       KC_4,          KC_5,          KC_6,          KC_EQL,        XXXXX,         KC_LSFT,       KC_LGUI,       KC_LALT,       KC_LCTL,       XXXXX,        \
-    XXXXX,         KC_GRV,        KC_1,          KC_2,          KC_3,          KC_BSLS,       XXXXX,         XXXXX,         XXXXX,         KC_ALGR,       XXXXX,         XXXXX,        \
-                                                 KC_DOT,        KC_0,          KC_MINS,       XXXXX,         XXXXX,         XXXXX
-   ),
+    XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX,         KC_LBRC,       KC_7,          KC_8,          KC_9,          KC_RBRC,      XXXXX,         \
+    XXXXX,         KC_LCTL,       KC_LALT,       KC_LGUI,       KC_LSFT,       XXXXX,         KC_SCLN,       KC_4,          KC_5,          KC_6,          KC_EQL,       XXXXX,         \
+    XXXXX,         XXXXX,         XXXXX,         KC_ALGR,       XXXXX,         XXXXX,         KC_GRV,        KC_1,          KC_2,          KC_3,          KC_BSLS,      XXXXX,         \
+                                                 XXXXX,         XXXXX,         XXXXX,         KC_DOT,        KC_0,          KC_MINS       
+    ),
+
     [LAYER_SYM] = LAYOUT(
     XXXXX,         KC_LCBR,       KC_AMPR,       KC_ASTR,       KC_LPRN,       KC_RCBR,       XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX,        \
     XXXXX,         KC_COLN,       KC_DLR,        KC_PERC,       KC_CIRC,       KC_PLUS,       XXXXX,         KC_LSFT,       KC_LGUI,       KC_LALT,       KC_LCTL,       XXXXX,        \
     XXXXX,         KC_TILD,       KC_EXLM,       KC_AT,         KC_HASH,       KC_PIPE,       XXXXX,         XXXXX,         XXXXX,         KC_ALGR,       XXXXX,         XXXXX,        \
                                                  KC_LPRN,       KC_RPRN,       KC_UNDS,       XXXXX,         XXXXX,         XXXXX
-   ),
+    ),
+
     [LAYER_FUN] = LAYOUT(
-    XXXXX,         KC_F12,        KC_F7,         KC_F8,         KC_F9,         KC_PSCR,       XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX,        \
-    XXXXX,         KC_F11,        KC_F4,         KC_F5,         KC_F6,         KC_SCRL,       XXXXX,         KC_LSFT,       KC_LGUI,       KC_LALT,       KC_LCTL,       XXXXX,        \
-    TG(LAYER_GAME),KC_F10,        KC_F1,         KC_F2,         KC_F3,         KC_PAUS,       XXXXX,         XXXXX,         XXXXX,         KC_ALGR,       XXXXX,         XXXXX,        \
-                                                 KC_APP,        KC_SPC,        KC_TAB,        XXXXX,         XXXXX,         XXXXX
-   ),
+    XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX,         KC_PSCR,       KC_F7,         KC_F8,         KC_F9,         KC_F12,        XXXXX,         \
+    XXXXX,         KC_LCTL,       KC_LALT,       KC_LGUI,       KC_LSFT,       XXXXX,         KC_SCRL,       KC_F4,         KC_F5,         KC_F6,         KC_F11,        TG(LAYER_GAME),\
+    XXXXX,         XXXXX,         XXXXX,         KC_ALGR,       XXXXX,         XXXXX,         KC_PAUS,       KC_F1,         KC_F2,         KC_F3,         KC_F10,        XXXXX,         \
+                                                 XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX,         XXXXX
+    ),
+
     [LAYER_GAME] = LAYOUT(
     KC_LEFT_CTRL,  KC_Q,          KC_W,          KC_E,          KC_R,          KC_T,          KC_Y,          KC_U,          KC_I,          KC_O,          KC_P,          KC_EQL,       \
     KC_LEFT_SHIFT, KC_A,          KC_S,          KC_D,          KC_F,          KC_G,          KC_H,          KC_J,          KC_K,          KC_L,          KC_SCLN,       KC_QUOT,      \
--- keyboards/crkbd/keymaps/yasakbulut/readme.md
+++ keyboards/crkbd/keymaps/yasakbulut/readme.md
@@ -11,6 +11,7 @@ This is a keymap heavily based off of miryoku with the following changes applied
 - Base layer right side extra keys from top to bottom are `='-`
 - Added backtick to the extra key next to Q
 - Nav layer bottom row pinkie and extra key are `\|` 
+- Rearranged the thumb row layer keys so that it is: FUN, NAV, NUM, MOUSE, GAME, MEDIA. This moves the number keys to the right hand, which is identical to a numpad. Removed SYM, since it's equivalent to NUM+SHIFT.
 
 - MacOS specific fixes:
   - Vertical mouse scroll keys are flipped
